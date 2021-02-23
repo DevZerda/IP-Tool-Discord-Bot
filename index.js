aec141f9fd472e58018a0728643f27b640b2f961
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-23 15:58:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const C = require("./Config/current.js");
 const Crud = require("./Auth/crud.js");
 const eCrud = require("./Auth/functions.js");
 const Extra = require("./Extra/functions.js");
-const { config } = require("process");
 
 // Extra Const/Vars Etc
 // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
@@ -24,9 +23,10 @@ const Client = new Discord.Client();
 
 Client.on('ready', () => {
     console.log(`Logged in as ${Client.user.tag}!\nServer Count: ${Client.guilds.cache.size}`);
-    Client.user.setActivity(Config.Info.Prefix + "help to start!");
-    // Client.user.setPresence({ game: { name: `the bot is working`, type: 'WATCHING' }, status: 'online' });
+    Client.user.setActivity(Config.Info.Prefix + "info to start!");
+    
 });
+
 
 Client.on('message', async (msg) => {
 
@@ -43,6 +43,7 @@ Client.on('message', async (msg) => {
 
     
     Config.GetMsg(msg.content);
+    
 
     /*
                         COMMAND HANDLER
@@ -51,6 +52,7 @@ Client.on('message', async (msg) => {
     if(C.CurrentCmd.Cmd === Config.Info.Prefix + "register") { 
         sendmsg("Register", Crud.userAdd(C.CurrentUser.Name, C.CurrentUser.UserID));
     } else if(eCrud.isRegistered(msg.author.id) && C.CurrentCmd.Cmd.startsWith(Config.Info.Prefix)) { // Register check
+        Extra.log_console("CMD");
         if(C.CurrentCmd.Cmd === Config.Info.Prefix + "info") {
             info_embed();
         } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "help") {
@@ -58,28 +60,87 @@ Client.on('message', async (msg) => {
         } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "myinfo") {
             sendmsg("My Info", "```" + eCrud.Userstats(C.CurrentUser.UserID) + "```");
         } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "geo") {
-            let test = await Extra.geoIP(C.CurrentCmd.arg[1]);
-            sendmsg("Geo Results", test);
+            if(C.CurrentCmd.argCount === 1) {
+                await geoResults(C.CurrentCmd.arg[1]);
+            } else {
+                sendmsg("Error", "Invalid argument\nUsage: " + Config.Info.Prefix + "geo <ip>");
+            }
         } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "botinv") {
             sendmsg("Bot Invite", "Spread the bot around!. " + Config.Info.Botinv);
         } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "serverinv") {
             sendmsg("Server Invite", Config.Info.ServerInv);
         } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "site") {
             sendmsg("Site", Config.Info.Site);
-        } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "stress") {
-            // sendmsg("Stress Status", "Attack sent to " + C.CurrentCmd.arg[1] + ":" + C.CurrentCmd.arg[2] + " for " + C.CurrentCmd.arg[3] + " seconds with " + C.CurrentCmd.arg[4]);
-            let api_resp = Extra.send_attack(C.CurrentCmd.arg[1], C.CurrentCmd.arg[2], C.CurrentCmd.arg[3], C.CurrentCmd.arg[4]);
-            if(api_resp.toLowerCase().includes("attack sent")) {
-                bootembed(C.CurrentCmd.arg[1], C.CurrentCmd.arg[2], C.CurrentCmd.arg[3], C.CurrentCmd.arg[4], Extra.currentTime());
+        } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "methods") {
+            sendmsg("Methods", "UDP\nTCP\nSTD")
+        } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "wipe") {
+            if(eCrud.isAdmin(C.CurrentUser.UserID)) {
+                if(C.CurrentCmd.argCount === 1) {
+                    if(C.CurrentCmd.arg[1] <= 1000) {
+                        let msg_c = C.CurrentCmd.arg[1];
+                        let n = 0;
+                        let c_s = true;
+                        while(c_s) {
+                            let left_over = parseInt(msg_c) - n;
+                            if(left_over <= 100) {
+                                msg.channel.bulkDelete(left_over);
+                                c_s = false;
+                            } else {
+                                msg.channel.bulkDelete(100);
+                            }
+                            n+=100;
+                        }
+                    } else {
+                        sendmsg("Error", "Invalid message amount. Max is 1000!")
+                    }
+                } else {
+                    sendmsg("Error", "Invalid argument\n Usage: " + Config.Info.Prefix + "wipe <amount>");
+                }
             } else {
-                sendmsg("Error", "Something went wrong sending attack (An admin must check backend)")
+                sendmsg("Error", "You aren't admin to use this command!");
             }
-        // } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "") {
-
+        } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "stress") {
+            if(C.CurrentCmd.arg.length > 3) {
+                if(eCrud.isPremium(C.CurrentUser.UserID) || eCrud.isAdmin(C.CurrentUser.UserID)) {
+                    Extra.send_attack(C.CurrentCmd.arg[1], C.CurrentCmd.arg[2], C.CurrentCmd.arg[3], C.CurrentCmd.arg[4]);
+                    bootembed(C.CurrentCmd.arg[1], C.CurrentCmd.arg[2], C.CurrentCmd.arg[3], C.CurrentCmd.arg[4], Extra.currentTime());
+                } else {
+                    sendmsg("Error", "You do not have premium to use this command!");
+                }
+            } else {
+                sendmsg("Error", "Invalid arguments\nUsage: " + Config.Info.Prefix + "stress <ip> <port> <time> <method>");
+            }
+        } else if(C.CurrentCmd.Cmd === Config.Info.Prefix + "admin") {
+            let tool = C.CurrentCmd.arg[1];
+            if(eCrud.isAdmin(C.CurrentUser.UserID)) {
+                if(tool == "servers") {
+                    let test = await servers();
+                    sendmsg("List of servers im in", "```" + test + "```");
+                } else if(tool == "server_invites") {
+                    let fag = "";
+                    Client.guilds.cache.forEach(guild => {
+                        guild.channels.cache.filter(x => x.type != "category").random().createInvite()
+                          .then(inv => console.log(guild.name + " | " + inv.url) );
+                    });
+                    console.log("\r\n");
+                    sendmsg("Invites", "Invites sent to console!");
+                } else if(tool == "search_user") {
+                    sendmsg("My Info", "```" + eCrud.Userstats(C.CurrentCmd.arg[2]) + "```");
+                } else if(tool == "update_user") {
+                    if(C.CurrentCmd.argCount === 5) {
+                        sendmsg("Update Status", Crud.userUpdate(C.CurrentCmd.arg[2], C.CurrentCmd.arg[3], C.CurrentCmd.arg[4], C.CurrentCmd.arg[5]))
+                    } else {
+                        sendmsg("Error", "Invalid arguments\nUsage: " + Config.Info.Prefix + "admin search_user <User_To_Update> <new lvl> <new maxtime> <new admin>");
+                    }
+                } else {
+                    adminhelp_embed();
+                }
+            } else {
+                sendmsg("Error", "You aren't admin to use this command");
+            }
         } else {
             sendmsg("Error", "That wasn't a command on the bot. type " + Config.Info.Prefix + "help for a list of commands!");
         }
-        Extra.log_console("CMD");
     } else if(C.CurrentCmd.Cmd.startsWith(Config.Info.Prefix)) {
         sendmsg("Error", "You aren't registered to use this bot!. Type ``" + Config.Info.Prefix + "register`` to use the bot!");
     } else {
@@ -89,7 +150,11 @@ Client.on('message', async (msg) => {
     /*
                         EXTRA FUNCTIONS
     */
-
+    
+    /*
+    *@param: Title, Description
+    *@type: (void)
+    */
     function sendmsg(title, desc) {
         const exampleEmbed = new Discord.MessageEmbed()
 	        .setColor('#ff0000')
@@ -99,6 +164,9 @@ Client.on('message', async (msg) => {
             msg.channel.send(exampleEmbed);
     }
 
+    /*
+    *@type: (void)
+    */
     function info_embed() {
         const exampleEmbed = new Discord.MessageEmbed()
 	        .setColor('#ff0000')
@@ -116,6 +184,9 @@ Client.on('message', async (msg) => {
             msg.channel.send(exampleEmbed);
     }
 
+    /*
+    *@type: (void)
+    */
     function help_embed() {
         const exampleEmbed = new Discord.MessageEmbed()
 	        .setColor('#ff0000')
@@ -128,6 +199,7 @@ Client.on('message', async (msg) => {
 		        { name: 'Server Invite | Invite to the main server', value: Config.Info.Prefix + 'serverinv'},
 		        { name: 'Site | Main web site to Pandemic', value: Config.Info.Prefix + 'site'},
 		        { name: 'My Info | My Statistics', value: Config.Info.Prefix + 'myinfo'},
+		        { name: 'Methods | List of methods', value: Config.Info.Prefix + 'methods'},
 		        { name: 'GeoIP | Geo locate an IP', value: Config.Info.Prefix + 'geoip <ip>'},
 		        { name: 'Port Scan | Port Scan a IP', value: Config.Info.Prefix + 'scan <ip>' },
 		        { name: 'Stresser | Smack a IP offline', value: Config.Info.Prefix + 'stress <ip> <port> <time> <method>'},
@@ -137,10 +209,63 @@ Client.on('message', async (msg) => {
             msg.channel.send(exampleEmbed);
     }
 
-    function bootembed(ip, p, t, m, status, timestamp) {
+    /*
+    *@type: (void)
+    */
+    function adminhelp_embed() {
+        const exampleEmbed = new Discord.MessageEmbed()
+	        .setColor('#ff0000')
+	        .setTitle(Config.Info.Title + " | Help")
+	        .setDescription('Command Info\nCommand Usage')
+	        .addFields(
+		        { name: 'Help | List of commands', value: Config.Info.Prefix + 'admin' },
+		        { name: 'Servers | List of server the bot is in', value: Config.Info.Prefix + 'admin servers'},
+		        { name: 'Invites | List of servers + invite', value: Config.Info.Prefix + 'admin server_invites'},
+		        { name: 'Servers | List of server the bot is in', value: Config.Info.Prefix + 'admin servers'},
+	        )
+	        .setFooter('Created/Developed By ' + Config.Info.Creator + ' | Headquarter Server: ' + Config.Info.ServerInv, ' ');
+            msg.channel.send(exampleEmbed);
+    }
+
+    /*
+    *@param: IP Address
+    *@type: (void)
+    */
+    async function geoResults(ip) {
+        let gg = await Extra.geoIP(ip);
+
+        let geo_info = gg.split("\n");
         const exampleEmbed = new Discord.MessageEmbed()
           .setColor('#ff0000')
-          .setTitle(Config.Info,Title + " | Attack Status")
+          .setTitle(Config.Info.Title + " | Geo Results")
+          .addFields(
+              // { name: 'Regular field title', value: 'Some value here' },
+              // { name: '\u200B', value: '\u200B' },
+              { name: 'Status', value: geo_info[0], inline: true },
+              { name: 'Country', value: geo_info[1], inline: true },
+              { name: 'Country US', value: geo_info[2], inline: true },
+              { name: 'Region', value: geo_info[3], inline: true },
+              { name: 'RegionName', value: geo_info[4], inline: true },
+              { name: 'City', value: geo_info[5], inline: true },
+              { name: 'Zip', value: geo_info[6], inline: true },
+              { name: 'Lat', value: geo_info[7], inline: true },
+              { name: 'Lon', value: geo_info[8], inline: true },
+              { name: 'Timezone', value: geo_info[9], inline: true },
+              { name: 'ISP', value: geo_info[10], inline: true },
+          )
+          .setFooter('Created/Developed By ' + Config.Info.Creator + ' | Headquarter Server: ' + Config.Info.ServerInv, ' ');
+  
+      msg.channel.send(exampleEmbed);
+    }
+
+    /*
+    *@param: IP Address, Port, Time, Method, Timestamp
+    *@type: (void)
+    */
+    function bootembed(ip, p, t, m, timestamp) {
+        const exampleEmbed = new Discord.MessageEmbed()
+          .setColor('#ff0000')
+          .setTitle(Config.Info.Title + " | Attack Status")
           .addFields(
               // { name: 'Regular field title', value: 'Some value here' },
               // { name: '\u200B', value: '\u200B' },
@@ -149,14 +274,40 @@ Client.on('message', async (msg) => {
               { name: 'Time', value: t, inline: true },
               { name: 'Method', value: m, inline: true },
               { name: 'Status', value: "True", inline: true },
-              { name: 'Timestamp', value: timestamp, inline: true },
+              { name: 'Timestamp', value: timestamp, inline: true }
           )
           .setImage('https://media1.giphy.com/media/lp7MBJt51Sb49gGLqD/giphy.gif')
           .setTimestamp()
           .setFooter('Created/Developed By ' + Config.Info.Creator + ' | Headquarter Server: ' + Config.Info.ServerInv, ' ');
   
-      message.channel.send(exampleEmbed);
+      msg.channel.send(exampleEmbed);
     }
+
+    /*
+    *@type: (Promise[<string>])
+    */
+    async function servers() {
+        let list = "";
+        await Client.guilds.cache.forEach(guild => {
+            list += `${guild.name} | ${guild.id}` + "\n";
+        })
+        return list;
+    }
+
+    /*
+    *@param: Server Name
+    *@type: (Promise[<string>])
+    */
+    async function get_server_id(server_name) {
+        let list = "";
+        await Client.guilds.cache.forEach(guild => {
+            if(guild.name === server_name) {
+                list += guild.id;
+            }
+        })
+        return list;
+    }
+
 })
 
 Client.login(Config.Info.Token);
