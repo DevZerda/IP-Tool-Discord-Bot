e68661672339efbd20bb3a22ca06557c9a79a9d3
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-23 08:16:33 -0500

    Create current.js

---
+++ Config/current.js
@@ -0,0 +1,30 @@
+
+/*
+*@type [<string>]
+*/
+exports.CurrentCmd = {
+    "Cmd": "",
+    "arg": [],
+    "fullcmd": ""
+};
+
+/*
+*@type [<string>]
+*/
+exports.CurrentUser = {
+    "Name": "",
+    "UserID": "",
+    "Level": 0,
+    "Maxtime": 0,
+    "Admin": false
+};
+
+/*
+*@type [<string>]
+*/
+exports.CurrentRoom = {
+    "Server": "",
+    "ServerID": "",
+    "Channel": "",
+    "ChannelID": ""
+};
