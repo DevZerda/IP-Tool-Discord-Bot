42b71962f2134bd630dc9786946dba6565089d49
Devious <74839733+DeviousTheDev@users.noreply.github.com>
2021-02-23 17:01:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const p = require('prompt-sync')();
 
-function start() {
     while(true) {
-        let inputCMD = p("project@pandemic_cp#~ ");
+        let inputCMD = p('project@pandemic_cp#~ ');
         if(inputCMD === "test") {
             console.log("working");
         }
     }
-}
